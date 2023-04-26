bb6fa63b7a5ac840a41f4b34c9dd7f4336ab84b0
Marco Exp <80280295+MarcoExpPer@users.noreply.github.com>
2023-04-26 12:37:15 +0200

    Removed API conection via URL

--- src/index.tsx
+++ src/index.tsx
@@ -17,32 +17,14 @@ const myURL = new URL(window.location.href);
 const perspectiveA = myURL.searchParams.get("perspective1");
 const perspectiveB = myURL.searchParams.get("perspective2");
 
-let apiURL = myURL.searchParams.get("apiURL");
-if (apiURL === null || apiURL === undefined) {
-  apiURL = config.API_URI;
-}
-
-
-let apiUSER = myURL.searchParams.get("apiUSER");
-if (apiUSER === null || apiUSER === undefined) {
-  apiUSER = config.API_USER;
-}
-
-let apiPASS = myURL.searchParams.get("apiPASS");
-if (apiPASS === null || apiPASS === undefined) {
-  apiPASS = config.API_PASS;
-}
-
-console.log(apiURL);
-
 root.render(
   //<React.StrictMode>
   <App
     perspectiveId1={perspectiveA}
     perspectiveId2={perspectiveB}
-    apiURL={apiURL}
-    apiUSER={apiUSER}
-    apiPASS={apiPASS}
+    apiURL={config.API_URI}
+    apiUSER={config.API_USER}
+    apiPASS={config.API_PASS}
   />
   //</React.StrictMode>
 );
